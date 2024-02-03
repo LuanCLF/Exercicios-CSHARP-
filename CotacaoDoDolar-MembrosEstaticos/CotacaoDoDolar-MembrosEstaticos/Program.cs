2bc67a5411b5d61c2b1cd2ae203895a9bed5c7fb
Luan Charlys <firminocharlys@gmail.com>
2024-02-03 20:50:06 -0300

    feat: exercicio da conversão de moeda

---
+++ CotacaoDoDolar-MembrosEstaticos/CotacaoDoDolar-MembrosEstaticos/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+using CotacaoDoDolar_MembrosEstaticos.domain;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Qual é a cotação do dólar? ");
+        double cotacao = double.Parse(Console.ReadLine()!);
+
+        Console.Write("Quantos dólares você vai comprar? ");
+        double quantia = double.Parse(Console.ReadLine()!);
+
+
+        double valor = conversor.ConversorDeMoeda(cotacao, quantia);
+
+        Console.WriteLine($"Valor a ser pago em reais = {valor:F2}");
+
+    }
+}
---
+++ CotacaoDoDolar-MembrosEstaticos/CotacaoDoDolar-MembrosEstaticos/domain/conversor.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace CotacaoDoDolar_MembrosEstaticos.domain
+{
+    public class conversor
+    {
+       private static double taxa = 0.06;  
+        
+        public static double ConversorDeMoeda(double cotacao, double quantia)
+        {
+            double valor = cotacao * quantia;
+            return valor + valor * taxa;
+        }
+
+
+    }
+}
