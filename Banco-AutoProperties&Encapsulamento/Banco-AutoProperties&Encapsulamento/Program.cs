1f39eb65a383dc9094598079e18a22967f866b63
Luan Charlys <firminocharlys@gmail.com>
2024-02-04 20:15:15 -0300

    feat: exercicio para treinar autoproperty e encapsulamento

---
+++ Banco-AutoProperties&Encapsulamento/Banco-AutoProperties&Encapsulamento/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+using Course.domain;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+
+        Console.Write("Entre o número da conta: ");
+        string number = Console.ReadLine()!;
+        Console.Write("Entre o titular da conta: ");
+        string name = Console.ReadLine()!;
+        Console.Write("Haverá depósito inicial(s/n)? ");
+        char response = char.Parse(Console.ReadLine()!);
+    
+        double initialDeposit = response == 's' ? double.Parse(Console.ReadLine()!) : 0.0;
+
+      BankUser user = new BankUser(number, name, initialDeposit);
+
+        Console.WriteLine("Dados da conta:");
+        Console.WriteLine(user);
+
+        Console.Write("Entre um valor para depósito: ");
+        double depositValue = double.Parse(Console.ReadLine()!);
+        user.Deposit(depositValue);
+
+        Console.WriteLine("Dados da conta atualizados:");
+        Console.WriteLine(user);
+
+        Console.Write("Entre um valor para saque: ");
+        double withdrawValue = double.Parse(Console.ReadLine()!);
+        user.Withdraw(withdrawValue);
+
+        Console.WriteLine("Dados da conta atualizados:");
+        Console.WriteLine(user);
+
+
+    }
+}
---
+++ Banco-AutoProperties&Encapsulamento/Banco-AutoProperties&Encapsulamento/domain/BankUser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace Course.domain;
+public class BankUser
+{
+
+    public string Number { get; private set; } = null!;
+
+    public string Name { get; private set; } = null!;
+    public double Balance { get; private set; }
+
+    public BankUser(string Number, string name)
+    {
+        Number = Number;
+        Name = name;
+    }
+
+    public BankUser(string Number, string name, double balance) : this(Number, name)
+    {
+
+        Deposit(balance);
+    }
+
+
+    public void Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+        }
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount > 0)
+        {
+            amount += 5;
+            Balance -= amount;
+        }
+    }
+
+    public override string ToString()
+    {
+
+        return $"Account {Number}, Holder: {Name}, Balance: R${Balance:F2}";
+
+
+    }
+
+}
+
+
+
+}
