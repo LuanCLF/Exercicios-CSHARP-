787f4368d8155009067794cf8c9bb7c20c329131
Luan Charlys <firminocharlys@gmail.com>
2024-02-05 15:15:21 -0300

    feat: exercicio de vetores

---
+++ Vetores-AluguelPensao/Vetores-AluguelPensao/Program.cs
@@ -0,0 +1,63 @@
+﻿using Vetores_AluguelPensao.domain;
+
+internal class Program
+{
+    private static void Main()
+    {
+
+        Tenant[] apartments = new Tenant[10];
+
+        Console.Write("Quantos quartos serão alugados? ");
+        int rentals = int.Parse(Console.ReadLine()!);
+
+        for (int i = 0; i < rentals; i++)
+        {
+            Console.Write("Qual seu nome? ");
+            string name = Console.ReadLine()!;
+
+            Console.Write("Qual seu email? ");
+            string email = Console.ReadLine()!;
+
+            Console.Write("Qual o quarto? ");
+            int room = CheckRoom(apartments);
+            int rent = i + 1;
+
+            apartments[room] = new Tenant(rent, name, email, room);
+           
+
+            Console.WriteLine($"Aluguel #{rent}");
+            Console.WriteLine($"Nome: {name}");
+            Console.WriteLine($"Email: {email}");
+            Console.WriteLine($"Quarto: {room}");
+
+        }
+
+
+       for(int i = 0; i < apartments.Length;i++)
+        {
+            if (apartments[i] != null) Console.WriteLine(apartments[i]);
+        }
+
+
+    }
+
+    private static int CheckRoom(Tenant[] apartments)
+
+
+    {
+        int room = int.Parse(Console.ReadLine()!);
+
+        if (apartments[room] == null) return room; 
+       
+        while (apartments[room] != null)
+        {
+            Console.Write("O quarto não está vago, informe outro: ");
+            room = int.Parse(Console.ReadLine()!);
+          
+        }
+        
+        return room;
+
+
+    }
+}
---
+++ Vetores-AluguelPensao/Vetores-AluguelPensao/domain/tenant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Vetores_AluguelPensao.domain
+{
+    public class Tenant
+    {
+        public int Rent { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public int Room { get; private set; }
+
+        public Tenant(int rent, string name, string email, int room)
+        {
+            Rent = rent;
+            Name = name;
+            Email = email;
+            Room = room;
+        }
+
+        public override string ToString()
+        {
+            return $"{Room}: {Name}, {Email}";
+        }
+    }
+}
