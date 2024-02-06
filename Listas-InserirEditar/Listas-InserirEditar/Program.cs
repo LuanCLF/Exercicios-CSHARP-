176c20cb8c8825c5f8cf6a3d1a5c867f4467da03
Luan Charlys <firminocharlys@gmail.com>
2024-02-06 15:58:56 -0300

    feat: exercicios de lista

---
+++ Listas-InserirEditar/Listas-InserirEditar/Program.cs
@@ -0,0 +1,59 @@
+﻿using Listas_InserirEditar.domain;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.Write("How many employees will be registered? ");
+        int amount = int.Parse(Console.ReadLine()!);
+
+        List<Functionary> functionaries = [];
+
+        for (int i = 0; i < amount; i++)
+        {
+            Console.Write("Id: ");
+            int id = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Name: ");
+            string name = Console.ReadLine()!;
+
+            Console.Write("Salary: ");
+            double salary = double.Parse(Console.ReadLine()!);
+
+
+            functionaries.Add(new Functionary(id, name, salary));
+        }
+
+        Increase(ref functionaries);
+    }
+
+    protected static void Increase(ref List<Functionary> functionaries)
+    {
+        Console.Write("Enter the employee id that will have salary increase: ");
+        int id = int.Parse(Console.ReadLine()!);
+
+        Functionary? theChose = functionaries.Find(x => x.Id == id);
+
+        if (theChose != null)
+        {
+            Console.Write("Enter the percentage: ");
+            double percentage = double.Parse(Console.ReadLine()!);
+
+            theChose.Increase(percentage);
+        }
+        else
+        {
+            Console.WriteLine("The ID does not exist");
+        }
+
+        Console.WriteLine("Updated list of employees:");
+
+        foreach (Functionary functionary in functionaries)
+        {
+            Console.WriteLine(functionary);
+        }
+
+    }
+
+
+}
---
+++ Listas-InserirEditar/Listas-InserirEditar/domain/Clerk.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Listas_InserirEditar.domain
+{
+    public class Functionary(int id, string name, double salary)
+    {
+        public int Id { get; private set; } = id;
+        public string Name { get; private set; } = name;
+        public double Salary { get; private set; } = salary;
+
+        public void Increase(double percentage)
+        {
+            Salary += Salary * (percentage / 100);
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Name}, {Salary:F2}";
+        }
+    }
+}
