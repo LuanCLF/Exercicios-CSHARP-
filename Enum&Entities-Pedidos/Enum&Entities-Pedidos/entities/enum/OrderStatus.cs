7fe8bee6f84b6d9fb5d3dd3e76d22490ef29c3ea
Luan Charlys <firminocharlys@gmail.com>
2024-02-10 22:18:43 -0300

    feat: enum OrderStatus add

---
+++ Enum&Entities-Pedidos/Enum&Entities-Pedidos/entities/enum/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Entities_Pedidos.entities.@enum
+{
+    enum OrderStatus
+{
+    PendingPayment,
+    Processing,
+    Shipped,
+    Delivered
+}
+}
--- Enum&Entities-Pedidos/Enum&Entities-Pedidos/obj/Debug/net8.0/Enum&Entities-Pedidos.AssemblyInfo.cs
+++ Enum&Entities-Pedidos/Enum&Entities-Pedidos/obj/Debug/net8.0/Enum&Entities-Pedidos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enum&Entities-Pedidos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97f52d0010b9bf4749f9efef1609561e10d2b1f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef26473b351df882739d0a3378ee32cab175d60c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enum&Entities-Pedidos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enum&Entities-Pedidos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
