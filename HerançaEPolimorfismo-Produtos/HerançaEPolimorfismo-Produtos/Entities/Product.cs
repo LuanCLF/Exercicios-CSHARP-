70f0f2b43f84411929a61931d0f7f6ff4ffb8e19
Luan Charlys <firminocharlys@gmail.com>
2024-02-14 19:36:34 -0300

    feat: create Product class

---
+++ HerançaEPolimorfismo-Produtos/HerançaEPolimorfismo-Produtos/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿
+namespace HerançaEPolimorfismo_Produtos.Entities
+{
+    public class Product
+    {
+        public string Name { get; private set; }
+
+        public double Price { get; private set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag()
+        {
+            return $"{Name} R${Price:F2}";
+        }
+    }
+}
